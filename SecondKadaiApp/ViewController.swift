bfd3c8481f338b3f95774a4167aed6f23359d79a
takumi.hatae <plazima0131@softbank.ne.jp>
2020-10-14 17:46:43 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by plazima on 2020/10/14.
+//  Copyright © 2020 takumi.hatae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
